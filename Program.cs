70dd6d0bd812e96596a7a6342f704bc69e05797c
Connor Robinson <connorrobinsontech@gmail.com>
2018-08-28 08:42:03 -0400

    Changed default notes directory. Also fixed some errors.

--- Program.cs
+++ Program.cs
@@ -1,64 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.IO;
 namespace NetCore
 {
-	class Program
-	{
-		static Random ran = new Random();
+    class Program
+    {
+        static Random ran = new Random();
 
-		static void Main(string[] args)
-		{
-			List<FlashCard> FlashCards =  ReadData.Read(@"/home/blue/FlashCardFiles");
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(System.Environment.CurrentDirectory, "Notes");
 
-			bool Quit = true;
+            List<FlashCard> FlashCards = ReadData.Read(filePath);
 
-			while (Quit)
-			{
-				if (FlashCards.Count > 0)
-				{
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.WriteLine("Definition-----------FlashCards v.1");
-					Console.ForegroundColor = ConsoleColor.Blue;
-					
-					string ifEsc = String.Empty;
-					int index = ran.Next(FlashCards.Count);
-					string Item = FlashCards[index].Item;
-					string Def = FlashCards[index].Definition;
+            bool Quit = true;
+            if (!File.Exists(filePath))
+            {
 
-					Console.WriteLine();
-					Console.WriteLine(Def);
-					Console.ForegroundColor = ConsoleColor.Red;
-					
-					ifEsc = Console.ReadLine();
-					if (ifEsc == ":q")
-					{
-						break;
-					}
-					Console.Clear();
+                while (Quit)
+                {
+                    if (FlashCards.Count > 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Definition-----------FlashCards v.1");
+                        Console.ForegroundColor = ConsoleColor.Blue;
 
-					Console.ForegroundColor = ConsoleColor.Green;
-					Console.WriteLine("Answer---------------FlashCards v.1");
-					Console.ForegroundColor = ConsoleColor.Magenta;
-					
-					Console.WriteLine();
-					Console.WriteLine(Item);
-					Console.ForegroundColor = ConsoleColor.Red;
-					ifEsc = Console.ReadLine();
-					if (ifEsc == ":q")
-					{
-						break;
-					}
+                        string ifEsc = String.Empty;
+                        int index = ran.Next(FlashCards.Count);
+                        string Item = FlashCards[index].Item;
+                        string Def = FlashCards[index].Definition;
 
-					Console.Clear();
-				}
-				else
-				{
-					Console.WriteLine("No Flash Cards Availible....");
-					Console.ReadKey();
-					break;
-				}
-			}
-		}
-	}
+                        Console.WriteLine();
+                        Console.WriteLine(Def);
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        ifEsc = Console.ReadLine();
+                        if (ifEsc == ":q")
+                        {
+                            break;
+                        }
+                        Console.Clear();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Answer---------------FlashCards v.1");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+
+                        Console.WriteLine();
+                        Console.WriteLine(Item);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        ifEsc = Console.ReadLine();
+                        if (ifEsc == ":q")
+                        {
+                            break;
+                        }
+
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No Flash Cards Availible....");
+                        Console.ReadKey();
+                        break;
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Notes Folder has been found :(");
+                Console.ReadKey();
+            }
+        }
+    }
 }
